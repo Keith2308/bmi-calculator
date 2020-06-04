f61f473273e545dc31014496ffa6e99d559d2410
Keith <ongkeeaik@gmail.com>
2020-06-04 11:07:07 +0800

    Updates

---
+++ precache-manifest.8197ec0501fc36b6ce9131a74713be24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738b181874b31189f75226e59269ebf0",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "46b3016d0a17f92435e0",
+    "url": "/bmi-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3bc1338d1cdab65af3be",
+    "url": "/bmi-calculator/static/css/main.6840af8e.chunk.css"
+  },
+  {
+    "revision": "46b3016d0a17f92435e0",
+    "url": "/bmi-calculator/static/js/2.13b83784.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bmi-calculator/static/js/2.13b83784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc1338d1cdab65af3be",
+    "url": "/bmi-calculator/static/js/main.ff9db19c.chunk.js"
+  },
+  {
+    "revision": "bf527aad9f06f8d8ce8a",
+    "url": "/bmi-calculator/static/js/runtime-main.06f24e2a.js"
+  }
+]);
